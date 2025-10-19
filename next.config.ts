4d9bb5b0cb0b8fb910806b7c5a9a3e0dd5b36258
elyulidev <elyuli.dev@gmail.com>
2025-10-19 23:07:17 +0300

    fix: delete empty string in remotePatters

--- next.config.ts
+++ next.config.ts
@@ -6,16 +6,11 @@ const nextConfig: NextConfig = {
 			{
 				protocol: "https",
 				hostname: "picsum.photos",
-				port: "",
 				pathname: "/seed/**",
-				search: "",
 			},
 			{
 				protocol: "https",
 				hostname: "cz7algaabcz8rpd1.public.blob.vercel-storage.com",
-				port: "",
-				pathname: "",
-				search: "",
 			},
 		],
 	},
